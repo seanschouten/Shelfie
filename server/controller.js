dcf3bf19332dd8410852e611b7f272e0a735cfb7
Sean Schouten <seanmschouten@gmail.com>
2019-06-07 15:49:08 -0600

    3:49pm

---
+++ server/controller.js
@@ -0,0 +1,8 @@
+module.exports = {
+create: (req, res, next) => {
+    const db = req.app.get('db')
+},
+read: (req, res, next) => {
+
+},
+}
--- server/index.js
+++ server/index.js
@@ -3,11 +3,18 @@ const express = require ('express');
 const massive = require('massive');
 const ctrl = require('./controller');
 const { SERVER_PORT , CONNECTION_STRING } = process.env
+const controller = require('./controller')
 
 const app = express();
 
 app.use(express.json())
 
+
+
+app.get('api/inventory', controller.read)
+app.get('/api/inventory', controller.create)
+app.delete('/api/inventory:id', controller.delete)
+app.put('/api/inventory:id', controller.update)
 
 massive(CONNECTION_STRING).then(db => {
     console.log('db is connected')
