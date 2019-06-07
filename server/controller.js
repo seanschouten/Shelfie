b45483596e6ca43278f98a7d476025004fe4b157
Sean Schouten <seanmschouten@gmail.com>
2019-06-07 16:10:07 -0600

    410pm

--- server/controller.js
+++ server/controller.js
@@ -1,8 +1,23 @@
 module.exports = {
 create: (req, res, next) => {
     const db = req.app.get('db')
+    const { productName, price, imageUrl } = req.body;
+    db.createProduct([ productName, price, imageUrl])
+    .then( () => res.send(inventory))
+    .cath(err => {res.status(500).send({errorMessage:'Not Working'});
+    console.log(err)
+});
 },
+
 read: (req, res, next) => {
 
 },
+
+update: (req, res, next) => {
+
+},
+
+delete: (req, res, next) =>{
+
+}
 }
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Product from '../Product/Product'
+import Axios from 'axios';
 
 export default class Dashboard extends Component {
    
